1e286c11912eadf5ec237abe1bdf48f82dcecf0c
mylezuiru <mylesuiru@gmail.com>
2022-11-22 21:33:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Myles Uiru'
+const welcomeMessage ="Welcome to Flatbook, ${Myles Uiru}!"
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ${MYLES UIRU}! .toUpperCase()!'
+const firstInitial =currentUser[0]
+const restOfName =currentUser.slice(1)
+const shortGreeting ="Welcome, ${M}!"
